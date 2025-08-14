d7ffc3dc9b6afb0cde1414486c6ffa5d17cba58b
AnaTorres88 <ana.torresgzzgmail.com>
2025-08-14 00:49:08 -0600

    Incluir primer test renderizar componente

---
+++ src/components/Calculator.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+</script>
+
+<template>
+    <section class="calculator">
+         <p> Efectua operaciones básicas como suma, resta, división, multiplicación, raíz cuadrada y potencia</p>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
