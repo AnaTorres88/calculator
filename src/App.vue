3a9c1afa644011c0923924a07c34a6cecb4ff589
AnaTorres88 <ana.torresgzzgmail.com>
2025-08-14 00:52:08 -0600

    añadir componente al dom

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-  //import Calculator from './components/Calculator.vue';
+  import Calculator from './components/Calculator.vue';
 </script>
 
 <template>
   <h1> Calculadora básica </h1>
-  <!-- <Calculator></Calculator> -->
+  <Calculator></Calculator>
 </template>
 
 <style scoped></style>
